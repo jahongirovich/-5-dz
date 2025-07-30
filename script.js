fb2ba11dc99afea9daa458abe66b1477f87214ba
jahongirovich <abdubositabduqahhorov2011@gmail.com>
2025-07-30 10:14:08 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const user = {
+    name: 'bosit',
+    id: '123456',
+    password: '123456'
+  };
+  
+  console.log(user);
+
+  const name = prompt("Введите имя:");
+const id = prompt("Введите ID:");
+const password = prompt("Введите пароль:");
+
+const user1 = {
+  name: name,
+  id: id,
+  password: password
+};
+
+console.log(user);
+  
